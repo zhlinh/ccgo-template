fd558904351354e0c345495a26a496590e7612c2
zhlinh <zhlinhng@gmail.com>
2025-04-03 20:43:32 +0800

    fix: add plugin alias

--- template/{{cpy_project_relative_path}}/build.gradle.kts
+++ template/{{cpy_project_relative_path}}/build.gradle.kts
@@ -15,6 +15,8 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.maven.publish) apply false
+    alias(libs.plugins.maven.nmcp) apply false
     // ccgo plugins
     alias(libs.plugins.ccgo.android.library.native.python) apply false
     alias(libs.plugins.ccgo.android.library.native.empty) apply false
